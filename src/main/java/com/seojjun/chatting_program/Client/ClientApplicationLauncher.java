2a91eafb7739cfc903b165662a12d15292b5a284
seojjun <sjm04210@gmail.com>
2024-06-08 02:48:11 +0900

    add jar files

---
+++ src/main/java/com/seojjun/chatting_program/Client/ClientApplicationLauncher.java
@@ -0,0 +1,7 @@
+package com.seojjun.chatting_program.Client;
+
+public class ClientApplicationLauncher {
+    public static void main(String[] args) {
+        ClientApplication.main(args);
+    }
+}
---
+++ src/main/java/com/seojjun/chatting_program/Server/ServerApplicationLauncher.java
@@ -0,0 +1,7 @@
+package com.seojjun.chatting_program.Server;
+
+public class ServerApplicationLauncher {
+    public static void main(String[] args) {
+        ServerApplication.main(args);
+    }
+}
